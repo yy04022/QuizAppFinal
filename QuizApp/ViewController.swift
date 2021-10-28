ced0a0902c7e6e8ca9f8ccfba0db21773b46e61d
Jinyu Wang <ellie5201314@gmail.com>
2021-10-27 21:02:08 -0400

    class exit

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trueButton: UIButton!
+   
+    @IBOutlet weak var falseButton: UIButton!
+    
+    
+    @IBOutlet weak var questions: UILabel!
+    
+    //2D Array - Two Dimensional Array
+    var questionBank = [["5 + 2 = 9","False"],
+                        ["9 - 2 = 8","False"],
+                        ["2 + 3 = 5","True"],
+                        ["1 - 1 = -1","False"]]
+    
+    var questionNumber = 0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI() //Calling function
+        
+        
+    }
+    
+    func updateUI(){
+        questions.text = questionBank[0][0]
+        
+        
+        
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
